492b06725dff9655c6dcd3acb18895ec4198ffd1
Joel Baxter <joel.baxter@neogeographica.com>
2018-04-07 00:04:41 -0700

    start on the additional "modern episodes"
    Still need to implement the cases for dmc3, unforgiven, and func_mapjam5.

--- installers/install_modern_episodes.bat
+++ installers/install_modern_episodes.bat
@@ -24,25 +24,37 @@ if not exist "%markv_exe%" (
 :menu
 cls
 call :installed_check oum
+call :installed_check rapture
+call :installed_check soe_full
 call :installed_check contract
 call :installed_check terra
 call :installed_check chapters
 call :installed_check travail
 call :installed_check warpspasm
+call :installed_check rmx-pack
 call :installed_check nsoe2
 call :installed_check arcanum
+call :installed_check dmc3
+call :installed_check unforgiven
 call :installed_check rrp
+call :installed_check func_mapjam5
 echo(
 echo "Modern" custom episodes to install:
 echo 1: oum - Operation: Urth Majik (2001)%oum_installed%
-echo 2: contract - Contract Revoked (2002)%contract_installed%
-echo 3: terra - Terra (2005)%terra_installed%
-echo 4: chapters - Contract Revoked: The Lost Chapters (2005)%chapters_installed%
-echo 5: travail - Travail (2007)%travail_installed%
-echo 6: warpspasm - Warp Spasm (2007)%warpspasm_installed%
-echo 7: nsoe2 - Soul of Evil: Indian Summer (2008)%nsoe2_installed%
-echo 8: arcanum - Arcanum (2011)%arcanum_installed%
-echo 9: rrp - Rubicon Rumble Pack (2014)%rrp_installed%
+echo 2: rapture - Rapture (2001)%rapture_installed%
+echo 3: soe_full - Soul of Evil (2002)%soe_full_installed%
+echo 4: contract - Contract Revoked (2002)%contract_installed%
+echo 5: terra - Terra (2005)%terra_installed%
+echo 6: chapters - Contract Revoked: The Lost Chapters (2005)%chapters_installed%
+echo 7: travail - Travail (2007)%travail_installed%
+echo 8: warpspasm - Warp Spasm (2007)%warpspasm_installed%
+echo 9: rmx-pack - Remix Map Pack (2008)%rmx-pack_installed%
+echo 10: nsoe2 - Soul of Evil: Indian Summer (2008)%nsoe2_installed%
+echo 11: arcanum - Arcanum (2011)%arcanum_installed%
+echo 12: dmc3 - Deathmatch Classics Vol. 3 (2011)%dmc3_installed%
+echo 13: unforgiven - Unforgiven (2011)%unforgiven_installed%
+echo 14: rrp - Rubicon Rumble Pack (2014)%rrp_installed%
+echo 15: func_mapjam5 - Func Map Jam 5 (2015)%func_mapjam5_installed%
 echo(
 set menu_choice=menu_exit
 set /p menu_choice=choose a number or just press Enter to exit:
@@ -59,6 +71,24 @@ pause
 goto :menu
 
 :2
+if exist rapture (
+  echo The "rapture" gamedir already exists.
+) else (
+  call "%~dp0\_mod_install.cmd" rapture
+)
+pause
+goto :menu
+
+:3
+if exist soe_full (
+  echo The "soe_full" gamedir already exists.
+) else (
+  call "%~dp0\_mod_install.cmd" soe_full
+)
+pause
+goto :menu
+
+:4
 if exist contract (
   echo The "contract" gamedir already exists.
 ) else (
@@ -67,7 +97,7 @@ if exist contract (
 pause
 goto :menu
 
-:3
+:5
 REM Terra doesn't normally have its own gamedir, but let's give it one
 if exist terra (
   echo The "terra" gamedir already exists.
@@ -81,7 +111,7 @@ if exist terra (
 pause
 goto :menu
 
-:4
+:6
 if exist chapters (
   echo The "chapters" gamedir already exists.
 ) else (
@@ -92,7 +122,7 @@ echo This is necessary even if you don't have missionpack 1 currently installed.
 pause
 goto :menu
 
-:5
+:7
 REM for Travail also install the soundtrack
 set quake_travail_soundtrack_markv_fix_success=
 if exist travail (
@@ -112,7 +142,7 @@ if "%quake_travail_soundtrack_markv_fix_success%"=="false" (
 pause
 goto :menu
 
-:6
+:8
 REM for Warp Spasm also install Quoth if necessary
 REM note that the "quoth" folder name is required for Quoth
 if not exist quoth (
@@ -138,7 +168,32 @@ echo Make sure to specify Quoth as the base game when playing "warpspasm".
 pause
 goto :menu
 
-:7
+:9
+REM Remix Map Pack doesn't normally have its own gamedir, but let's give it one
+if exist rmx-pack (
+  echo The "rmx-pack" gamedir already exists.
+) else (
+  call "%~dp0\_mod_install.cmd" rmx-pack
+  md rmx-pack 2> nul
+  md rmx-pack\gfx 2> nul
+  md rmx-pack\gfx\env 2> nul
+  md rmx-pack\maps 2> nul
+  move id1\*rmx*.txt rmx-pack > nul
+  move id1\gfx\env\elbrus256*.tga rmx-pack\gfx\env > nul
+  move id1\maps\*rmx*.* rmx-pack\maps > nul
+  move id1\maps\start.bsp rmx-pack\maps > nul
+  REM delete these unneeded files
+  del /q id1\fitz-rmx.bat
+  del /q id1\gnu.txt
+  del /q id1\mx.txt
+  REM delete these uncommon dirs if empty
+  rd id1\gfx\env 2> nul
+  rd id1\gfx 2> nul
+)
+pause
+goto :menu
+
+:10
 if exist nsoe2 (
   echo The "nsoe2" gamedir already exists.
 ) else (
@@ -147,7 +202,7 @@ if exist nsoe2 (
 pause
 goto :menu
 
-:8
+:11
 if exist arcanum (
   echo The "arcanum" gamedir already exists.
 ) else (
@@ -157,12 +212,29 @@ if exist arcanum (
 pause
 goto :menu
 
-:9
+:12
+echo TODO
+echo maybe patch from http://quaddicted.com/filebase/dmc3m67qw.zip
+pause
+goto :menu
+
+:13
+echo TODO
+pause
+goto :menu
+
+:14
 if exist rrp (
   echo The "rrp" gamedir already exists.
 ) else (
   call "%~dp0\_mod_install.cmd" rrp
 )
+pause
+goto :menu
+
+:15
+echo TODO
+echo patch from http://www.quaddicted.com/files/mods/QuickerQonquer.zip
 pause
 goto :menu
 
