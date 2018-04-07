528e352c9a236f390ced3540517c26453026131c
Joel Baxter <joel.baxter@neogeographica.com>
2018-04-07 10:27:39 -0700

    additional "classic" episodes

--- installers/install_classic_episodes.bat
+++ installers/install_classic_episodes.bat
@@ -23,16 +23,20 @@ if not exist "%markv_exe%" (
 
 :menu
 cls
+call :installed_check prodigy_se
 call :installed_check bbelief
 call :installed_check mexx9
 call :installed_check zer
+call :installed_check descent
 call :installed_check nehahra
 echo(
 echo "Classic" custom episodes to install:
-echo 1: bbelief - Beyond Belief (1997)%bbelief_installed%
-echo 2: mexx9 - Penumbra of Domination (1997)%mexx9_installed%
-echo 3: zer - Zerstorer (1997)%zer_installed%
-echo 4: nehahra - Nehahra (2000)%nehahra_installed%
+echo 1: prodigy_se - Prodigy Special Edition (1997)%prodigy_se_installed%
+echo 2: bbelief - Beyond Belief (1997)%bbelief_installed%
+echo 3: mexx9 - Penumbra of Domination (1997)%mexx9_installed%
+echo 4: zer - Zerstorer (1997)%zer_installed%
+echo 5: descent - (The Final) Descent (2000)%descent_installed%
+echo 6: nehahra - Nehahra (2000)%nehahra_installed%
 echo(
 set menu_choice=menu_exit
 set /p menu_choice=choose a number or just press Enter to exit:
@@ -40,6 +44,15 @@ echo(
 goto %menu_choice%
 
 :1
+if exist prodigy_se (
+  echo The "prodigy_se" gamedir already exists.
+) else (
+  call "%~dp0\_mod_install.cmd" prodigy_se
+)
+pause
+goto :menu
+
+:2
 REM for Beyond Belief also install the patch
 set bbelief6_fix_success=
 if exist bbelief (
@@ -59,7 +72,7 @@ if "%bbelief6_fix_success%"=="false" (
 pause
 goto :menu
 
-:2
+:3
 if exist mexx9 (
   echo The "mexx9" gamedir already exists.
 ) else (
@@ -68,7 +81,7 @@ if exist mexx9 (
 pause
 goto :menu
 
-:3
+:4
 REM for Zerstorer also install the patches
 set zer11_success=
 set zerend_fix_success=
@@ -97,7 +110,16 @@ if "%good_zer_patches%"=="false" (
 pause
 goto :menu
 
-:4
+:5
+if exist descent (
+  echo The "descent" gamedir already exists.
+) else (
+  call "%~dp0\_mod_install.cmd" descent
+)
+pause
+goto :menu
+
+:6
 if exist nehahra (
   echo The "nehahra" gamedir already exists.
 ) else (
