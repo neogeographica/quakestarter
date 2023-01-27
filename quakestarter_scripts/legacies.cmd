1e3a00d2a4034666f163d17b5dc391433d3c16f3
Joel Baxter <joel.baxter@neogeographica.com>
2023-01-26 17:52:24 -0800

    handle legacies moves from other_post_quoth
    arwop and its_demo_v1_1
    
    part of issue #82

--- quakestarter_scripts/legacies.cmd
+++ quakestarter_scripts/legacies.cmd
@@ -39,7 +39,9 @@ call :installed_check rpgsmse rating
 call :installed_check sm82 rating
 call :installed_check ant rating
 call :installed_check fmb_bdg rating
+call :installed_check arwop rating
 call :installed_check dmc3 rating
+call :installed_check its_demo_v1_1
 call :installed_check retrojam4dlc_pulsar rating
 call :installed_check plaw02 rating
 call :installed_check markiesm1v2 rating
@@ -111,30 +113,36 @@ if "%show_rating%"=="true" (
   if "%show_fmb_bdg%"=="true" (
     echo %is_fmb_bdg_installed%  7: fmb_bdg - This Onion ^(2007^)
   )
+  if "%show_arwop%"=="true" (
+    echo %is_arwop_installed%  8: arwop - A Roman Wilderness Of Pain ^(2009^)
+  )
   if "%show_dmc3%"=="true" (
-    echo %is_dmc3_installed%  8: dmc3 - Deathmatch Classics Vol. 3 ^(2011^)
+    echo %is_dmc3_installed%  9: dmc3 - Deathmatch Classics Vol. 3 ^(2011^)
+  )
+  if "%show_its_demo_v1_1%"=="true" (
+    echo %is_its_demo_v1_1_installed% 10: its_demo_v1_1 - In The Shadows [Demo v1.1] ^(2012^)
   )
   if "%show_retrojam4dlc_pulsar%"=="true" (
-    echo %is_retrojam4dlc_pulsar_installed%  9: retrojam4dlc_pulsar - The Elder Reality ^(2016^)
+    echo %is_retrojam4dlc_pulsar_installed% 11: retrojam4dlc_pulsar - The Elder Reality ^(2016^)
   )
   if "%show_plaw02%"=="true" (
-    echo %is_plaw02_installed% 10: plaw02 - Waldsterben ^(2022^)
+    echo %is_plaw02_installed% 12: plaw02 - Waldsterben ^(2022^)
   )
   if "%show_markiesm1v2%"=="true" (
-    echo %is_markiesm1v2_installed% 11: markiesm1v2 - Slip Tripping ^(2022^)
+    echo %is_markiesm1v2_installed% 13: markiesm1v2 - Slip Tripping ^(2022^)
   )
   echo.
 )
 if "%show_version%"=="true" (
   echo Dropped because superseded by a newer version:
   if "%show_copper_v1_19%"=="true" (
-    echo %is_copper_v1_19_installed% 12: copper_v1_19 - Copper 1.19 ^(2022^)
+    echo %is_copper_v1_19_installed% 14: copper_v1_19 - Copper 1.19 ^(2022^)
   )
   if "%show_ctsj2%"=="true" (
-    echo %is_ctsj2_installed% 13: ctsj2 - Coppertone Summer Jam 2 v1.0 ^(2022^)
+    echo %is_ctsj2_installed% 15: ctsj2 - Coppertone Summer Jam 2 v1.0 ^(2022^)
   )
   if "%show_toneodspmp3_2_3%"=="true" (
-    echo %is_toneodspmp3_2_3_installed% 14: toneodspmp3_2_3 - Empire of Disorder v2.3 ^(2022^)
+    echo %is_toneodspmp3_2_3_installed% 16: toneodspmp3_2_3 - Empire of Disorder v2.3 ^(2022^)
   )
   echo.
 )
@@ -218,8 +226,18 @@ set start_map=fmb_bdg1
 call "%scriptspath%_handle_mod_choice.cmd" fmb_bdg
 goto :menu
 
-REM Deathmatch Classics Vol. 3 should age out six months after v3.8.0
+REM A Roman Wilderness Of Pain should age out six months after v3.8.0
 :8
+if not "%show_arwop%"=="true" (
+  goto :eof
+)
+set start_map=start
+set startdemos=demo1
+call "%scriptspath%_handle_mod_choice.cmd" arwop
+goto :menu
+
+REM Deathmatch Classics Vol. 3 should age out six months after v3.8.0
+:9
 if not "%show_dmc3%"=="true" (
   goto :eof
 )
@@ -228,8 +246,21 @@ set start_map=dmc3
 call "%scriptspath%_handle_mod_choice.cmd" dmc3
 goto :menu
 
+REM In The Shadows [Demo v1.1] should age out six months after v3.8.0
+:10
+if not "%show_its_demo_v1_1%"=="true" (
+  goto :eof
+)
+set start_map=start
+set skip_quakerc_gen=true
+set prelaunch_msg[0]=This mod supports some unique stealth gameplay; be sure to check the
+set prelaunch_msg[1]=readme for tips!
+set prelaunch_msg[2]=
+call "%scriptspath%_handle_mod_choice.cmd" its_demo_v1_1
+goto :menu
+
 REM The Elder Reality should age out after 4/6/2023
-:9
+:11
 if not "%show_retrojam4dlc_pulsar%"=="true" (
   goto :eof
 )
@@ -238,7 +269,7 @@ call "%scriptspath%_handle_mod_choice.cmd" retrojam4dlc_pulsar
 goto :menu
 
 REM Waldsterben should age out after 4/6/2023
-:10
+:12
 if not "%show_plaw02%"=="true" (
   goto :eof
 )
@@ -254,7 +285,7 @@ call "%scriptspath%_handle_mod_choice.cmd" plaw02
 goto :menu
 
 REM Slip Tripping should age out after 4/6/2023
-:11
+:13
 if not "%show_markiesm1v2%"=="true" (
   goto :eof
 )
@@ -263,7 +294,7 @@ call "%scriptspath%_handle_mod_choice.cmd" markiesm1v2
 goto :menu
 
 REM Copper 1.19 should age out six months after v3.8.0
-:12
+:14
 if not "%show_copper_v1_19%"=="true" (
   goto :eof
 )
@@ -273,7 +304,7 @@ call "%scriptspath%_handle_mod_choice.cmd" copper_v1_19
 goto :menu
 
 REM Coppertone Summer Jam 2 v1.0 should age out after 2/11/2023
-:13
+:15
 if not "%show_ctsj2%"=="true" (
   goto :eof
 )
@@ -283,7 +314,7 @@ call "%scriptspath%_handle_mod_choice.cmd" ctsj2
 goto :menu
 
 REM Empire of Disorder v2.3 should age out six months after v3.8.0
-:14
+:16
 if not "%show_toneodspmp3_2_3%"=="true" (
   goto :eof
 )
